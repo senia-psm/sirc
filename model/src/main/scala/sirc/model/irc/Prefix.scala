00487e4014e2e44e3176c49d56472a9f689d7715
senia-psm <seniapsm@gmail.com>
2012-11-17 20:42:27 +0400

    Update model/src/main/scala/sirc/model/irc/Prefix.scala

--- model/src/main/scala/sirc/model/irc/Prefix.scala
+++ model/src/main/scala/sirc/model/irc/Prefix.scala
@@ -11,7 +11,7 @@ sealed abstract class Prefix { def toIrc: String }
 case class SimplePrefix(name: String) extends Prefix { val toIrc: String = name } //server name or nickname
 
 case class Nickname(name: String, user: Option[String] = None, host: Option[String] = None) extends Prefix {
-  require(user == None || host != None, "Can't handle user without host.")
+  require(user == None || host != None, "Can't handle host without user.")
 
   lazy val toIrc: String = name + user.map("!" + _).mkString + host.map("@" + _).mkString
 }
